e902e55ecc3d72d61fe5f247e1cf231230140975
TechRouterBoy <449177540@qq.com>
2026-08-28 22:07:31 +0800

    Add test comment in diy-part1.sh
    Added a new test comment to the script.

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,3 +4,4 @@ cd openwrt
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 # 屏蔽系统自带的开源mt76无线驱动
 # sed -i '/mt76/d' package/kernel/linux/modules/wireless.mk
+#new test
