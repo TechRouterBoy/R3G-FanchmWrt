b05936ab423007be89babcce4675a4bf3247a87f
TechRouterBoy <449177540@qq.com>
2026-08-28 22:31:19 +0800

    Delete diy-part1.sh

--- diy-part1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd openwrt
-# 修改路由器后台IP为192.168.5.1
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-# 屏蔽系统自带的开源mt76无线驱动
-# sed -i '/mt76/d' package/kernel/linux/modules/wireless.mk
-#new test
