aed60eb2555c905e1e66e184fcebf145ee6677af
TechRouterBoy <449177540@qq.com>
2026-08-28 20:49:11 +0800

    Comment out feeds update for small
    Comment out the feed update command for small.

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,5 +6,5 @@ git clone https://github.com/hanwckf/mt76-openwrt package/mt76
 wget https://raw.githubusercontent.com/sonytxl/Xiaomi-R3G/master/mt7621.conf -O package/mt76/src/mt7621.conf
 
 # 安装TTYD网页终端、DDNSTO内网穿透
-./scripts/feeds update small
+# ./scripts/feeds update small
 ./scripts/feeds install luci-app-ttyd luci-app-ddnsto
